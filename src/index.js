7deace4d6e3e1660c7cba0d9c344e78e58fe7f04
custertian <custertian@users.noreply.github.com>
2017-07-11 22:16:05 +0800

    最简易的版本
    在ace-editor.js里实现了AceEditor

--- src/index.js
+++ src/index.js
@@ -2,39 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './registerServiceWorker';
-import brace from 'brace';
-import AceEditor from 'react-ace';
-import 'brace/mode/javascript';
-import 'brace/mode/python';
-import 'brace/mode/matlab';
-import 'brace/theme/github';
-/*eslint-disable no-alert, no-console */
-import 'brace/ext/language_tools';
-import 'brace/snippets/javascript';
-import 'brace/snippets/python';
-import 'brace/snippets/matlab';
-// import 'brace/ext/searchbox';
+
+import MyAceEditor from './component/ace-editor'
 
 // Render editor
 ReactDOM.render(
-  <AceEditor
-    mode="python"
-    theme="github"
-    name="UNIQUE_ID_OF_DIV"
-    editorProps={{$blockScrolling: true}}
-    fontSize={16}
-    showPrintMargin={true}
-    showGutter={true}
-    highlightActiveLine={true}
-    setOptions={{
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-        showLineNumbers: true,
-        tabSize: 4,
-    }}
-  />,
-  document.getElementById('editor')
+  <MyAceEditor/>,
+  document.getElementById('root')
 );
 
 
