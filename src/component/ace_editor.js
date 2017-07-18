ff9eadeb9316c7c89a320ceaa57405cb8a5e95a4
custertian <custertian@users.noreply.github.com>
2017-07-18 09:51:01 +0800

    简版
    简版

--- src/component/ace_editor.js
+++ src/component/ace_editor.js
@@ -1,27 +1,15 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 // import brace from 'brace';
 import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
-import 'brace/mode/python';
-import 'brace/mode/matlab';
 import 'brace/theme/github';
 /*eslint-disable no-alert, no-console */
 import 'brace/ext/language_tools';
 import 'brace/snippets/javascript';
-import 'brace/snippets/python';
-import 'brace/snippets/matlab';
 // import 'brace/ext/searchbox';
 import Button from 'antd/lib/button';
 import '../css/style.css'
-
-const defaultValue = 
-`function setup(){
-    createCanvas(100,100);
-}
-function draw(){
-    ellipse(20,20,20,20);
-}`
 
 /**
  * 实现AceEditor功能
@@ -29,95 +17,73 @@ function draw(){
  * 'brace/theme/github'         - 主题
  * 'brace/snippets/javascript'  - 代码段
  */
-class MyIframe extends React.Component{
-    updateIFrameContents() {
-        
-    }
-    
-    componentDidMount() {
-        // 组件渲染完成之后
-        var iFrameNode = this.refs.sketchFrame
-        var frameDoc = iFrameNode.contentWindow.document
-        frameDoc.write(`<!doctype html>
-<html>
-
-<head>
-
-    <script language="javascript" type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.11/p5.min.js"></script>
-    
-    <style>
-        html,
-        body {
-            overflow: hidden;
-            margin: 0;
-            padding: 0;
-            background: white;
-        }
-    </style>
-</head>
-
-<body>
-</body>
-
-</html>`)
-        this.updateIFrameContents()
-    }
-    componentDidUpdate(){
-        this.updateIFrameContents()
-    }
-    render(){
-        return <iframe title="unique"  ref = "sketchFrame" {...this.props}/>
-    }
-}
-class MyAceEditor extends React.Component {  
-    constructor(props){
+class MyAceEditor extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
-            value: defaultValue,
+        this.state = {
+            value: this.props.defaultValue,
+            _isMounted: false
         }
-    } 
+        this.onChange = this.onChange.bind(this)
+    }
+    onChange(newValue) {
+        this.setState({
+            value: newValue
+        })
+        // console.log("value", this.state.value)
+
+    }
     executeCode(event) {
         event.preventDefault()// 阻止默认行为
-        var sketchFrame = document.getElementById('bannerIframe')
-        var code = this.state.value
-        code += `\n new p5();\n`
-        this.setState({
-            value: code
-        })
-        var userScript = sketchFrame.contentWindow.document.createElement('script')
+        let myiframe = document.getElementById("myiframe")
+        var iwindow = ReactDOM.findDOMNode(myiframe).contentWindow
+        var idoc = ReactDOM.findDOMNode(myiframe).contentDocument
+        var userScript = idoc.createElement('script')
         userScript.type = 'text/javascript'
-        userScript.text = code
-        userScript.async = false
-        sketchFrame.contentWindow.document.body.appendChild(userScript)
-        // this.updateIFrameContents(sketchFrame.src)        
+        userScript.text = this.state.value + `\n new p5();`
+        userScript.async = true
+        iwindow.document.body.appendChild(userScript)
+        this.initialIframe()
     }
-    updateIFrameContents(src){
-        // document.getElementById('bannerIframe').src = src
+
+    initialIframe() {
+        var iFrameNode = this.refs.sketchFrame
+        var frameDoc = iFrameNode.contentWindow.document
+        frameDoc.write(`<!doctype html><html><head><script language="javascript" type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.11/p5.min.js"></script></head><body></body></html>`)
+        frameDoc.close()
     }
-    
     render() {
         return (
             <div>
-            <AceEditor
-                mode="javascript"
-                theme="github"
-                name="UNIQUE_ID_OF_DIV"
-                editorProps={{ $blockScrolling: true }}
-                fontSize={16}
-                showPrintMargin={true}
-                showGutter={true}
-                highlightActiveLine={true}
-                value={this.state.value}
-                setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: true,
-                    showLineNumbers: true,
-                    tabSize: 4,
-                }}
-            />
-            <Button type="primary" shape="circle" icon="caret-right" size="large" onClick={this.executeCode.bind(this)} />
-            <MyIframe ref="bannerIframe" id="bannerIframe" title="sketchFrame" width="600px" height="400px"/>
+                <iframe
+                    id="myiframe"
+                    ref="sketchFrame"
+                    title="myiframe"
+                    frameBorder="0"
+                    width="100%"
+                    height="400"
+                    src="./_sketch.html">
+                </iframe>
+                <AceEditor
+                    mode="javascript"
+                    theme="github"
+                    onChange={this.onChange}
+                    name="yak_editor"
+                    editorProps={{ $blockScrolling: true }}
+                    fontSize={16}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value={this.state.value}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        enableSnippets: true,
+                        showLineNumbers: true,
+                        tabSize: 4,
+                    }}
+                />
+                <Button type="primary" shape="circle" icon="caret-right" size="large" onClick={this.executeCode.bind(this)} />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import registerServiceWorker from './registerServiceWorker';
+import MyAceEditor from './component/ace_editor'
 
-import ViewEditor from './component/view_editor'
+/**
+ *  MyAceEditor 组件的使用和默认的展示代码
+ */
+const defaultCode = 
+`
+function setup() {
+  createCanvas(720, 400);
+}
 
+function draw() {
+  background(127);
+  noStroke();
+  for (var i = 0; i < height; i += 20) {
+    fill(129, 206, 15);
+    rect(0, i, width, 10);
+    fill(255);
+    rect(i, 0, 10, height);
+  }
+}
+`
 // Render editor
 ReactDOM.render(
-  <ViewEditor/>,
+  <MyAceEditor defaultValue={defaultCode}/>,
   document.getElementById('root')
 );
-
-
-registerServiceWorker();
